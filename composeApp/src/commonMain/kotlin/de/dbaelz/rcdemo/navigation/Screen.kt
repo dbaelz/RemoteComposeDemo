f6ca6d4ef4a9adba603d745ceff2b221ea6d6519
Daniel Bälz <info@dbaelz.de>
2026-03-20 17:35:38 +0100

    Use Remote Compose with Column to display content as proof of concept

--- composeApp/src/commonMain/kotlin/de/dbaelz/rcdemo/navigation/Screen.kt
+++ composeApp/src/commonMain/kotlin/de/dbaelz/rcdemo/navigation/Screen.kt
@@ -2,7 +2,7 @@ package de.dbaelz.rcdemo.navigation
 
 enum class Screen(val title: String, val actions: Set<Action> = emptySet()) {
     HelloWorld(
-        title = "HelloWorld",
+        title = "Hello World",
         actions = setOf(Action.HelloWorldRefresh)
     )
 }
--- server/src/main/kotlin/de/dbaelz/rcdemo/HelloWorldRemote.kt
+++ server/src/main/kotlin/de/dbaelz/rcdemo/HelloWorldRemote.kt
@@ -1,9 +1,13 @@
 package de.dbaelz.rcdemo
 
+import androidx.compose.remote.core.operations.layout.managers.BoxLayout
 import androidx.compose.remote.core.operations.layout.managers.CoreText
 import androidx.compose.remote.creation.JvmRcPlatformServices
 import androidx.compose.remote.creation.RemoteComposeWriter
 import androidx.compose.remote.creation.modifiers.RecordingModifier
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 class HelloWorldRemote {
     private var counter = 1
@@ -14,20 +18,40 @@ class HelloWorldRemote {
         val writer = RemoteComposeWriter(JvmRcPlatformServices())
 
         writer.root {
-            writer.textComponent(
-                RecordingModifier().fillMaxSize(),
-                writer.addText("Hello World! \nCounter: ${counter++}"),
-                0xFF757575L.toInt(),
-                128f,
-                0,
-                400f,
-                null,
-                CoreText.TEXT_ALIGN_START,
-                0,
-                Int.MAX_VALUE
-            ) {}
+            writer.column(
+                RecordingModifier()
+                    .fillMaxSize()
+                    .padding(32),
+                BoxLayout.CENTER,
+                BoxLayout.TOP
+            ) {
+                text(writer, "Hello World", 48.dp)
+
+                text(writer, "Call counter: ${counter++}", 24.dp)
+            }
         }
 
         return writer.encodeToByteArray()
+    }
+
+    private fun text(writer: RemoteComposeWriter, text: String, fontSize: Dp) {
+
+        writer.textComponent(
+            RecordingModifier(),
+            writer.addText(text),
+            0xFF757575L.toInt(),
+            fontSize.value * density.density,
+            0,
+            400f,
+            null,
+            CoreText.TEXT_ALIGN_START,
+            0,
+            Int.MAX_VALUE
+        ) {}
+    }
+
+    private companion object {
+        // Fixed value, should be handed over by calling client?
+        val density = Density(3f)
     }
 }
